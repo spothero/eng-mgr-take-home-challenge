9f5f5ad7f4fe3ae79db5248024ba2d9f004550b2
Attila Domokos <adomokos@gmail.com>
2021-01-26 15:22:20 -0600

    Initial commit

---
+++ sql/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS worked_hours;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  manager_id INT,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT true,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE worked_hours (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  date DATE NOT NULL,
+  hours NUMERIC(4,2) NOT NULL CHECK (hours > 0 AND hours <= 24),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  CONSTRAINT fk_users
+    FOREIGN KEY(user_id)
+    REFERENCES users(id)
+);
