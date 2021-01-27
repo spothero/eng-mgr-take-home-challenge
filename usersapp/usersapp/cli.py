8eb4f25ef1a1e5a13a72d8947b985ee7bb6261b7
Attila Domokos <adomokos@gmail.com>
2021-01-26 22:28:17 -0600

    Get started with logic

--- usersapp/usersapp/cli.py
+++ usersapp/usersapp/cli.py
@@ -1,5 +1,22 @@
+from .db import session_scope
+from .models import User
+
+
+Result = int
+
+
+def register_user(user: User) -> Result:
+    pass
+
+
+def send_welcome_message(email: str) -> None:
+    with session_scope() as session:
+        u = session.query(User).filter(User.email == email).first()
+        print(f"user is {u.__dict__}")
+
+
 def run(args=None):
     """
     Runs the app from the command line
     """
-    print("Hello")
+    send_welcome_message("jsmith@example.com")
