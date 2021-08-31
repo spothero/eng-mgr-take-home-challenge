5f589facf34593a56517eee3a5d558041ad29b02
navreddy23 <89608934+navreddy23@users.noreply.github.com>
2021-08-31 13:03:20 -0500

    fixing test constants (#14)

--- src/test/java/testUtils/TestConstants.java
+++ src/test/java/testUtils/TestConstants.java
@@ -5,7 +5,7 @@ public class TestConstants {
   public static final String SERVICE_UP =  "{\n  \"id\" : 1,\n  \"content\" : \"pong\"\n}";
   public static final String POST_HOURS_SUCCESS = "worked hours posted successfully";
   public static final String POST_HOURS_FAILED = "posting worked hours failed";
-  public static final String HOURS_ALREADY_RECORDED = "user has already recorded"
+  public static final String HOURS_ALREADY_RECORDED = "user has already recorded "
       + "worked hours for this day";
   public static final String NO_USER = "user does not exist in the system";
 }
